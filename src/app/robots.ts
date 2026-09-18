50c955d13b7be980e19e4c864ffc343ef6372cea
UNICON LEATHER <export@myunicon.com>
2026-09-18 18:56:22 +0530

    Update canonical domain and SEO metadata to www.uniconleather.com

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/", "/_next/"],
     },
-    sitemap: "https://myunicon.com/sitemap.xml",
+    sitemap: "https://www.uniconleather.com/sitemap.xml",
   };
 }
