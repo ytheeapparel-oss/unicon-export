1168ba364c214c1fa0dafa63d2c3017b1a5ef29e
UNICON LEATHER <export@myunicon.com>
2026-09-19 10:51:03 +0530

    Configure global search engine crawler directives and IndexNow verification key

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,34 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/api/", "/_next/"],
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/_next/"],
+      },
+      {
+        userAgent: [
+          "Googlebot",
+          "Googlebot-Image",
+          "Bingbot",
+          "YandexBot",
+          "Baiduspider",
+          "Applebot",
+          "DuckDuckBot",
+          "Yeti",
+          "SeznamBot",
+          "Qwantify",
+          "Slurp",
+          "facebookexternalhit",
+          "Twitterbot",
+          "LinkedInBot",
+          "Pinterestbot",
+        ],
+        allow: "/",
+      },
+    ],
     sitemap: "https://myuniconcom.vercel.app/sitemap.xml",
+    host: "https://myuniconcom.vercel.app",
   };
 }
